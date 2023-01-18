32f1d1b1aa334082c4f61c032be3a442bd6f4e97
yaty1031 <yaty1031@gmail.com>
2023-01-18 22:25:27 +0700

    fix: remove unused Import

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
